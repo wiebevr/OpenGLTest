480c7c794c1c0dddc4c8ca202eebcfd92ff8ec2f
plomplot <wiebevr@gmail.com>
2010-12-21 16:45:25 +0100

    full screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <QApplication>
 #include "widget.h"
+#include <QWidget>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QWidget parentWidget;
+    QVBoxLayout *layout = new QVBoxLayout(&parentWidget);
+    parentWidget.showFullScreen();
+
     Widget widget;
-    widget.show();
+    layout->addWidget(&widget);
+    parentWidget.show();
 
     
     return app.exec();
