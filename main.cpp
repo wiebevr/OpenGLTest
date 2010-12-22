99422662dd9e7a0780a43cfa7be39723b56babfa
plomplot <wiebevr@gmail.com>
2010-12-22 14:51:48 +0100

    Removed parent widge

--- main.cpp
+++ main.cpp
@@ -6,14 +6,18 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+#ifdef PARENT_WIDGET
     QWidget parentWidget;
     QVBoxLayout *layout = new QVBoxLayout(&parentWidget);
     parentWidget.showFullScreen();
-
+#endif
     Widget widget;
+#ifdef PARENT_WIDGET
     layout->addWidget(&widget);
     parentWidget.show();
-
+#else
+    widget.show();
+#endif
     
     return app.exec();
 }
